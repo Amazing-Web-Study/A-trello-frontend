4cf859c80211f98bed0b868c999a9118b34c00fd
sowish23 <soso2266@naver.com>
2021-08-09 20:11:25 +0900

    add example view and component

---
+++ src/components/ExampleComponent/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExampleComponent: React.FC = () => {
+  return (
+      <div>컴포넌트 예시</div>
+  );
+}
+
+export default ExampleComponent;
---
+++ src/views/Example/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ExampleComponent from '../../components/ExampleComponent/';
+
+const Example: React.FC = () => {
+  return (
+      <div>
+          여기는 페이지
+          <ExampleComponent />
+          <ExampleComponent />
+          <ExampleComponent />
+          <ExampleComponent />
+          <ExampleComponent />
+      </div>
+  );
+}
+
+export default Example;
