b747956bfee9293ff2c5c8001c91b41b55fefcd7
sowish23 <soso2266@naver.com>
2021-08-18 20:26:12 +0900

    ✨ add cardlist to board

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./index.css";
+import CardList from '../../components/CardList/'
 
 
 const Board: React.FC = () => {
+    const [data, setData] = useState([
+      {
+        id: 1,
+        title: "To-Do",
+        card: [{
+            title: 'test',
+            description: 'test1'
+          }, {
+            title: 'test2',
+            description: 'test12' 
+          }, { 
+            title: 'test3', 
+            description: 'test13' 
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: "Working",
+        card: [{
+            title: 'test',
+            description: 'test1'
+          }, {
+            title: 'test2',
+            description: 'test12' 
+          }, { 
+            title: 'test3', 
+            description: 'test13' 
+          }
+        ]
+      },
+      {
+        id: 3,
+        title: "Done",
+        card: [{
+            title: 'test',
+            description: 'test1'
+          }, {
+            title: 'test2',
+            description: 'test12' 
+          }, { 
+            title: 'test3', 
+            description: 'test13' 
+          }
+        ]
+      }
+    ]);
     return (
         <div className="board">
-
+            {
+            data.map((e, i) => {
+                return (
+                <CardList key={i} title={e.title} card={e.card} />
+                )
+            })
+            }
         </div>
-);
+    );
 }
 
 export default Board;
