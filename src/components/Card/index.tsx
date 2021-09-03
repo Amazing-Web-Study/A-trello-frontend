b99bc4301f7add950566c99272b3fbcf8b6a4476
firemanCha <vaite714@gmail.com>
2021-09-03 20:19:24 +0900

    수정: Card/index.tsx / 수정 버튼 추가

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import "./Card.css";
 
 interface CardProps {
-  title: string;
-  description: string;
+  title: string | undefined;
+  description: string | undefined;
 };
 
 const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
     <div className="card-wrapper">
       <div className="card">
-        <div className="card-title">{title}</div>
+        <div className="card-title">
+          {title}
+          <div className="card-title-wrapper">
+            <button>
+              @
+            </button>
+          </div>
+        </div>
         <div className="card-description">{description}</div>
       </div>
     </div>
