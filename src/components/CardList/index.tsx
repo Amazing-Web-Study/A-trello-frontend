be7ac062843cbbe01b8f4da63e8d475c57b4c308
firemanCha <vaite714@gmail.com>
2021-09-20 19:34:29 +0900

    수정: CardList/index.tsx / title div -> input 태그 변경 및 title update api 추가

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './index.css'
 import Card from '../Card'
 import axios from 'axios';
+import { setSyntheticTrailingComments } from 'typescript';
 
 interface CardProps {
     title: string;
@@ -15,24 +16,35 @@ interface CardListProps {
     deleteCard: any;
     deleteList: any;
     addCard: any;
+    updateCard: any;
+    updateList: any;
 }
 
 const CardList: React.FC<CardListProps> = (props) => {
+    const [list_title, setTitle] = useState<any>();
+
+    useEffect(() => {
+        setTitle(props.title);
+    }, [])
 
     return (
         <div className="cardlist">
             <div className="cardlist-title">
-                <div className="cardlist-title-text">{props.title}</div>
+                <input type="text" value={list_title || ''} onChange={(e) => setTitle(e.target.value)} onBlur={(e) => {
+                    e.preventDefault();
+                    props.updateList(props._id, list_title);
+                }} />
+                {/* <div className="cardlist-title-text">{props.title}</div> */}
                 <div className="cardlist-title-menu">
                     <div className="cardlist-title-menu-icon">+</div>
-                    <div onClick={() => props.deleteList(props._id)}>x</div>
+                    <div style={{ cursor: "pointer" }} onClick={() => props.deleteList(props._id)}>x</div>
                 </div>
             </div>
             <div className="cardlist-list">
                 {
                     props.card?.map((e: any, i: any) => {
                         return (
-                            <Card key={i} list_id={props._id} id={e._id} title={e.title} description={e.description} deleteCard={props.deleteCard} />
+                            <Card key={i} list_id={props._id} id={e._id} title={e.title} description={e.description} deleteCard={props.deleteCard} updateCard={props.updateCard} />
                         )
                     })
                 }
