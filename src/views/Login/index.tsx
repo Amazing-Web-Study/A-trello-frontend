1d84a47f9806bb8c637b156b65fab6f531ef4a53
sowish23 <soso2266@naver.com>
2021-09-20 18:40:53 +0900

    add signup api

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -5,7 +5,11 @@ const Example: React.FC = () => {
     return (
         <div className="login">
             <div className="login-box">
-                <div className="login-box-title">Log in to A-Trello</div>
+                <img
+                    alt="Trello"
+                    className="trello-main-logo"
+                    src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/167dc7b9900a5b241b15ba21f8037cf8/trello-logo-blue.svg"
+                />
                 <div className="login-box-input-box">
                     <div className="login-box-input"><input type="text" placeholder="Enter email"/></div>
                     <div className="login-box-input"><input type="text" placeholder="Enter password"/></div>
--- src/views/Register/index.tsx
+++ src/views/Register/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import Button from "../../components/Button";
 import './index.css'
 import TextInput from "../../components/TextInput";
+import axios from "axios";
 
 const Register: React.FC = () => {
+    const history = useHistory();
+    const[email, setEmail] = useState()
+    const[password, setPassword] = useState()
+    const[name, setName] = useState()
+
+    const addUser = async() => {
+        await axios.post('http://localhost:3030/user', {
+            id: email,
+            password,
+            name
+        }).then((res: any) => {
+            console.log(res);
+            alert('회원가입이 완료 되었습니다!')
+            history.push('/login')
+        });
+    }
+
     return (
         <div className={'inner-section'}>
             <img
@@ -16,9 +35,10 @@ const Register: React.FC = () => {
                     <h1 className={'signup-h1'}>
                         Sign up for your account
                     </h1>
-                    <TextInput placeholder={"Enter email"} style={{marginBottom: '20px'}}/>
-                    <TextInput placeholder={"Enter full name"} style={{marginBottom: '20px'}}/>
-                    <Button disabled={true} text={"Continue"}/>
+                    <TextInput placeholder={"Enter email"} style={{marginBottom: '20px'}} onChange={setEmail}/>
+                    <TextInput placeholder={"Enter password"} style={{marginBottom: '20px'}} onChange={setPassword}/>
+                    <TextInput placeholder={"Enter full name"} style={{marginBottom: '20px'}} onChange={setName}/>
+                    <Button disabled={false} text={"Continue"} onclick={addUser}/>
                     <hr/>
 
                     <span className={'bottom-form-link'}>
