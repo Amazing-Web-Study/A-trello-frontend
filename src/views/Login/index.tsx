7cea5f043734c4ad986024d68c7616910e511abf
sowish23 <soso2266@naver.com>
2021-09-20 19:34:10 +0900

    로그인 토큰 확인 로그아웃 기능 추가

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 import "./Login.css";
+import axios from "axios";
 
 const Example: React.FC = () => {
+    const history = useHistory();
+    const[email, setEmail] = useState<string>()
+    const[password, setPassword] = useState<string>()
+
+    useEffect(() => {
+        const checkToken = async() => {
+            await axios.post('http://localhost:3030/user/token/', {
+                token: localStorage.getItem('token')
+            }).then((res: any) => {
+                console.log(res);
+                history.push('/')
+            }).catch((err: any) => {
+                console.log(err);
+            });
+        }
+    checkToken()
+    }, [])
+
+    const login = async() => {
+        await axios.post('http://localhost:3030/login/', {
+            id: email,
+            password
+        }).then((res: any) => {
+            console.log(res)
+            localStorage.setItem('token', res.data.keyValue._id)
+            alert('로그인 성공!')
+            history.push('/')
+        }).catch((err: any) => {
+            console.log(err)
+            alert('아이디와 비밀번호를 확인해주세요')
+            setEmail('')
+            setPassword('')
+        });
+    }
     return (
         <div className="login">
             <div className="login-box">
@@ -11,9 +47,15 @@ const Example: React.FC = () => {
                     src="https://d2k1ftgv7pobq7.cloudfront.net/meta/c/p/res/images/trello-header-logos/167dc7b9900a5b241b15ba21f8037cf8/trello-logo-blue.svg"
                 />
                 <div className="login-box-input-box">
-                    <div className="login-box-input"><input type="text" placeholder="Enter email"/></div>
-                    <div className="login-box-input"><input type="text" placeholder="Enter password"/></div>
-                    <div className="login-box-btn"><button>Log In</button></div>
+                    <div className="login-box-input">
+                        <input type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="login-box-input">
+                        <input type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                    <div className="login-box-btn">
+                        <button onClick={() => login()}>Log In</button>
+                    </div>
                 </div>
                 <div className="login-box-signup">Sign Up</div>
             </div>
